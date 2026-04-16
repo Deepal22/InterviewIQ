3290593829283297c2fb8fdd7fe24307fb952f00
Deepal Tembhurne <tembhurnedeepal@gmail.com>
2026-04-16 22:15:17 +0530

    Fix deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,9 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [ENV.CLIENT_URL,
+  "http://localhost:5173"], 
+  credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
@@ -30,18 +32,15 @@ app.get("/health", (req, res) => {
 });
 
 // make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+})
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+    const PORT = ENV.PORT || 3000;
+    app.listen(PORT, () => console.log("Server is running on port:", PORT));
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
