d720b5103ab6ed84f1aeff85b398ce5594d66d50
Deepal Tembhurne <tembhurnedeepal@gmail.com>
2026-04-17 22:38:35 +0530

    Commit

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -4,6 +4,13 @@ import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/token", protectRoute, getStreamToken);
+router.get("/token", protectRoute, async (req, res) => {
+  try {
+    // dummy test first
+    res.json({ token: "test-token" });
+  } catch (error) {
+    res.status(500).json({ message: "Error generating token" });
+  }
+});
 
 export default router;
